94f570056a34ce158b47c943a6e800b6e0f5891e
g-dotcom <Gourav.its@gmail.com>
2023-02-13 18:11:40 +0530

    first release

---
+++ server/main.js
@@ -0,0 +1,40 @@
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+import { TasksCollection } from '/imports/db/TasksCollection';
+import '/imports/api/tasksMethods';
+import '/imports/api/tasksPublications';
+
+
+const SEED_USERNAME = 'gourav';
+const SEED_PASSWORD = 'mona';
+
+const insertTask = (taskText,user) => 
+  TasksCollection.insert({ 
+    text: taskText,
+    userId: user._id,
+    createdAt: new Date(), 
+  });
+
+Meteor.startup(() => {
+  if (!Accounts.findUserByUsername(SEED_USERNAME)) {
+    Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  }
+
+  const user = Accounts.findUserByUsername(SEED_USERNAME);
+
+  
+  if (TasksCollection.find().count() === 0) {
+    [
+      'First Task',
+      'Second Task',
+      'Third Task',
+      'Fourth Task',
+      'Fifth Task',
+      'Sixth Task',
+      'Seventh Task'
+    ].forEach(taskText => insertTask(taskText, user));
+  }
+});
